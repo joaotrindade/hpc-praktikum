3e3495c6802b46158fac63790024444cb1dece31
João Trindade <joao.ferreira.trindade@gmail.com>
2015-11-04 12:15:16 +0100

    Added Serial Version for Exercise 1 - Tutorial 2

---
+++ tutorial_2/exercise 1/serial.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+
+	double area, pi, x;
+	int i, n;
+	char c;
+
+	cout << "Number of divisions ? "; 
+	cin >> n; 
+	area = 0.0;
+
+	for (i = 0; i < n; i++) {
+	   x = (i+0.5)/n;
+	   area += 1.0/(1.0 + x*x);
+	}
+	
+	pi = area * 4 / n;
+
+	cout << setprecision(18) << "PI = " << pi << endl << endl;
+	
+	cout << "Press enter to continue ...";
+	cin.get(c);
+	cin.get(c);
+		
+}
