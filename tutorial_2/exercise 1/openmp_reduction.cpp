3e52ed94a54ac423a26342e2e8b6b856ffe7c6ca
João Trindade <joao.ferreira.trindade@gmail.com>
2015-11-04 14:46:21 +0100

    added time measurement

--- tutorial_2/exercise 1/openmp_reduction.cpp
+++ tutorial_2/exercise 1/openmp_reduction.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include "timer.h"
 
 using namespace std;
 
@@ -11,6 +12,7 @@ int main (int argc, char *argv[])
 	double area, pi, x;
 	int i, n;
 	char c;
+	time_marker_t time;
 	cout << "Available processors: " << omp_get_num_procs() << endl;
 	cout << "Number of threads ? ";
 	cin >> n;
@@ -20,13 +22,13 @@ int main (int argc, char *argv[])
 	cin >> n; 
 	area = 0.0;
 
+	time = get_time();
 	#pragma omp parallel for private(x) reduction(+:area)
 	for (i = 0; i < n; i++) {
 	   x = (i+0.5)/n;
 	   area += 1.0/(1.0 + x*x);
 	}
-	
 	pi = area * 4 / n;
-
+	printf("Time elapsed. time: %f   ticks: %f\n", get_ToD_diff_time(time), get_ticks_diff_time(time));		
 	cout << setprecision(18) << "PI = " << pi << endl;
 }
--- tutorial_2/exercise 1/serial.cpp
+++ tutorial_2/exercise 1/serial.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include "timer.h"
 
 using namespace std;
 
@@ -10,17 +11,17 @@ int main (int argc, char *argv[])
 	double area, pi, x;
 	int i, n;
 	char c;
-
+	time_marker_t time;
 	cout << "Number of divisions ? "; 
 	cin >> n; 
 	area = 0.0;
-
+	
+	time = get_time();
 	for (i = 0; i < n; i++) {
 	   x = (i+0.5)/n;
 	   area += 1.0/(1.0 + x*x);
 	}
-	
 	pi = area * 4 / n;
-
-	cout << setprecision(18) << "PI = " << pi << endl;		
+	printf("Time elapsed. time: %f   ticks: %f\n", get_ToD_diff_time(time), get_ticks_diff_time(time));		
+	cout << setprecision(18) << "PI = " << pi << endl;
 }
