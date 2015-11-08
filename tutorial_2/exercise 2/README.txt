cb31c88f6044e0923046dad223d5faf6004be36c
João Trindade <joao.ferreira.trindade@gmail.com>
2015-11-08 16:19:05 +0100

    added missing run scripts

---
+++ tutorial_2/exercise 2/README.txt
@@ -0,0 +1,5 @@
+1- $make all
+use run_cache_16_core.sh for exercise 2.2
+use run_cache_8_core.sh for exercise 2.3
+use run_remote_test.sh for exercise 2.4
+use run_cache_test.sh for exercise 2.5
--- tutorial_2/exercise 2/run_remote_test.sh
+++ tutorial_2/exercise 2/run_remote_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export KMP_AFFINITY="granularity=core,explicit,proclist=[0,1,2,3,8,9,10,11],verbose"
+export KMP_AFFINITY="granularity=core,explicit,proclist=[0,8,9,10,11,1,2,3],verbose"
 ./stream_omp_remote.o
 
