6880d8d99c3534d41904136730c42cdcec47674b
João Trindade <joao.ferreira.trindade@gmail.com>
2015-11-15 15:45:10 +0100

    first commit

--- tutorial_3/exercise3/broadcast.cpp
+++ tutorial_3/exercise3/broadcast.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <mpi.h>
+
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
+
+	int rank, size, error;
+	MPI_Init( &argc, &argv );
+	MPI_Comm_size( MPI_COMM_WORLD, &size );
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	
+	if (rank == 0 ) {
+		cout << "main process" << endl;
+	}
+	else {
+		cout << "rank: " << rank << endl;	
+	}
 
 	return 0;
 }
