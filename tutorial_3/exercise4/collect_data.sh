eeeb239290e4f0ac67e259b91754e5243fb7f350
LRZ-SIM Funktionskennung <t1221an@mpp2-login5.cos.lrz.de>
2015-11-23 00:52:18 +0100

    result

--- tutorial_3/exercise4/collect_data.sh
+++ tutorial_3/exercise4/collect_data.sh
@@ -5,22 +5,6 @@ export OMP_NUM_THREADS=22
 #salloc --ntasks=80
 
 
-echo -e "\nserial time for ngrid_points_per_dimension=5000:" >> performance_analysis.txt
-./serial_app 5000 1000 0.0001
-echo -e "\ngrid_points_per_dimension=5000:" >> performance_analysis.txt
-mpirun -np 1 ./mpi_app 5000 1000 0.0001 1 1
-mpirun -np 2 ./mpi_app 5000 1000 0.0001 2 1
-mpirun -np 4 ./mpi_app 5000 1000 0.0001 2 2
-mpirun -np 9 ./mpi_app 5000 1000 0.0001 3 3
-mpirun -np 16 ./mpi_app 5000 1000 0.0001 4 4
-mpirun -np 20 ./mpi_app 5000 1000 0.0001 5 4
-mpirun -np 36 ./mpi_app 5000 1000 0.0001 6 6
-mpirun -np 49 ./mpi_app 5000 1000 0.0001 7 7
-mpirun -np 81 ./mpi_app 5000 1000 0.0001 9 9
-mpirun -np 169 ./mpi_app 5000 1000 0.0001 13 13
-mpirun -np 289 ./mpi_app 5000 1000 0.0001 17 17
-
-
 echo -e "\nserial time for ngrid_points_per_dimension=3000:" >> performance_analysis.txt
 ./serial_app 3000 1000 0.0001
 echo -e "\ngrid_points_per_dimension=3000:" >> performance_analysis.txt
--- tutorial_3/exercise4/run.sh
+++ tutorial_3/exercise4/run.sh
@@ -2,10 +2,6 @@
 source /etc/profile.d/modules.sh
 export OMP_NUM_THREADS=22
 #cd $HOME/hpc-praktikum/tutorial_3/exercise3
-#salloc --ntasks=40
-./serial_app 5000 1000 0.0001
-#mpirun -np 64 ./mpi_app 1994 1000 0.0001 8 8
-mpirun -np 64 ./mpi_app 5000 1000 0.0001 8 8
-#mpirun -np 4 ./mpi_app 998 1000 0.0001 2 2
-
-#mpirun -np 16 ./mpi_app 998 1000 0.0001 4 4
+#salloc --ntasks=80
+./serial_app 2000 1000 0.0001
+mpirun -np 196 ./mpi_app 2000 1000 0.0001 14 14
