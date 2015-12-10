3c0611925ef12cf9d469a1677dd7bc1e0d13c255
LRZ-SIM Funktionskennung <mantoshkumar1@gmail.com>
2015-12-11 00:41:07 +0100

    some code

--- tutorial_4/exercise4/collect_data.sh
+++ tutorial_4/exercise4/collect_data.sh
@@ -5,7 +5,17 @@ export OMP_NUM_THREADS=22
 prblm_size=1000
 while [ "$prblm_size" -lt 5010 ]
 do
+   echo -e "\n problem size(naive algo) = $prblm_size"
+   echo -e "\n *****************************************************************************"
    perf stat -e cache-misses,L1-dcache-load-misses,LLC-load-misses,dTLB-load-misses  ./naive_app $prblm_size
+
+   echo -e "\n Perf: problem size(optimized algo) = $prblm_size"
+   echo -e "\n *****************************************************************************"
    perf stat -e cache-misses,L1-dcache-load-misses,LLC-load-misses,dTLB-load-misses  ./optimized_app $prblm_size
+
+   echo -e "\n valgrind: problem size(optimized algo) = $prblm_size"
+   echo -e "\n *****************************************************************************"
+   valgrind --tool=cachegrind ./optimized_app $prblm_size
+
    prblm_size=`expr $prblm_size + 1000 `
 done
--- tutorial_4/exercise4/run.sh
+++ tutorial_4/exercise4/run.sh
@@ -2,6 +2,6 @@
 source /etc/profile.d/modules.sh
 export OMP_NUM_THREADS=22
 #cd $HOME/hpc-praktikum/tutorial_3/exercise3
-salloc --partition=bdz --ntasks=400
+salloc --partition=bdz --ntasks=100
 #mpirun -np 196 ./mpi_app 2000 1000 0.0001 14 14
 #mpirun -np 196 ./mpi_app 2000 1000 0.0001 14 14
