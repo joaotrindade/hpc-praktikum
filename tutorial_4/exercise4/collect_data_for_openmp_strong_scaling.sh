fcb9ee306058412890062ec948796cb57a4e4153
LRZ-SIM Funktionskennung <mantoshkumar1@gmail.com>
2015-12-11 06:38:24 +0100

    done

---
+++ tutorial_4/exercise4/collect_data_for_openmp_strong_scaling.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+prblm_size=3000
+block_size=16
+num_th=4
+
+while [ "$num_th" -lt 35 ]
+do
+   export OMP_NUM_THREADS=$num_th
+   perf stat -e instructions,cache-misses,branch-misses,page-faults,cpu-migrations,stalled-cycles-frontend,stalled-cycles-backend,L1-dcache-load-misses,L1-dcache-prefetch-misses,LLC-prefetch-misses,dTLB-load-misses,branch-load-misses ./openmp_app $prblm_size $block_size
+   num_th=`expr $num_th "*" 2 `
+done
