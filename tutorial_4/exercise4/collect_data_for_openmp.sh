b60e0faa89e01e3330f3b7487c429d9e1ed68c40
LRZ-SIM Funktionskennung <mantoshkumar1@gmail.com>
2015-12-11 02:03:42 +0100

    bug

--- tutorial_4/exercise4/collect_data_for_openmp.sh
+++ tutorial_4/exercise4/collect_data_for_openmp.sh
@@ -6,7 +6,7 @@ num_th=1
 while [ "$num_th" -lt 35 ]
 do
    export OMP_NUM_THREADS=$num_th
-   ./parallelize.out $prblm_size $block_size
+   perf stat -e cache-misses,branch-misses,bus-cycles,cpu-clock,page-faults,context-switches,L1-dcache-load-misses,L1-dcache-prefetch-misses,LLC-load-misses,LLC-prefetch-misses,dTLB-load-misses,branch-load-misses ./openmp_app $prblm_size $block_size
    num_th=`expr $num_th "*" 2 `
    prblm_size=`expr $prblm_size "*" 2 `
 done
--- tutorial_4/exercise4/openmp_dgemm.c
+++ tutorial_4/exercise4/openmp_dgemm.c
@@ -31,25 +31,12 @@ int main(int argc, char **argv)
 	int i, j, k;
 	int ii, jj, kk;
 	 
-	double para_time_result,
-	       seq_time_result;
+	double para_time_result;
 
-	double speedup,
-	       eff;
 
 	int block_size = 16;
 	int num_thread = 0;
 
-	//char logfile_name[1000];
-	FILE *log_non_openmp_time_vs_prblmSize,   /* for non-OpenMP : Problem size vs time */
-	     *log_openmp_prblmSize_vs_time,       /* for OpenMP : Problem size vs time */
-	     *log_openmp_time_vs_threads,         /* for OpenMP: time vs num of threads */
-	     *log_openmp_speedup_vs_threads,      /* for OpenMP: speedup vs number of threads */
-	     *log_openmp_eff_vs_threads,          /* for OpenMP: Efficiency vs num of threads */ 
-	     *log_ideal_speedup,                  /* for OpenMP: ideal speedup */
-	     *log_ideal_eff,                      /* for OpenMP: ideal efficiency */
-	     *log_openmp_eff_vs_prblm_size,       /* for OpenMP: eff vs problem size */
-	     *log_openmp_speedup_vs_prblm_size;   /* for OpenMP: speedup vs problem size */
 
 	n = 500; /* default problem size */
 	double flops;
@@ -61,21 +48,6 @@ int main(int argc, char **argv)
 	if(argc > 2){
 		block_size = atoi(argv[2]);
 	}
-
-
-
-	//sprintf(logfile_name, "logfile_dgemm.txt");
-	
-	/* Opening file descriptors */
-	log_non_openmp_time_vs_prblmSize = fopen("./generated_data/log_non_opemp_time_vs_prblmSize.txt", "a+");
-	log_openmp_prblmSize_vs_time = fopen("./generated_data/log_openmp_time_prlm_sz.txt", "a+");
-	log_openmp_time_vs_threads = fopen("./generated_data/log_openmp_time_vs_threads.txt", "a+");
-	log_openmp_speedup_vs_threads = fopen("./generated_data/log_openmp_speedup_vs_threads.txt", "a+");
-	log_openmp_eff_vs_threads = fopen("./generated_data/log_openmp_eff_vs_threads.txt", "a+");
-	log_ideal_speedup = fopen("./generated_data/log_openmp_ideal_speedup.txt", "a+");
-	log_ideal_eff = fopen("./generated_data/log_openmp_ideal_eff.txt", "a+");
-	log_openmp_eff_vs_prblm_size = fopen("./generated_data/log_openmp_eff_vs_prblm_sz.txt", "a+");
-	log_openmp_speedup_vs_prblm_size = fopen("./generated_data/log_openmp_speedup_vs_prblm_sz.txt", "a+");
 
 
 
@@ -100,29 +72,8 @@ int main(int argc, char **argv)
 
 	memset(c, 0, mem_size);
 
-	/* matrix multiplication with Non-OpenMP starts here */
-	time_marker_t seq_time = get_time();
-	for(i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
-			for(k = 0; k < n; k++){
-				c[i * n + j] += a[i * n + k] * b[k * n + j];
-			}
-		}
-	}
-
-	flops = 2.0 * n * n * n;
-
-	printf("Non OpenMP: Problem size = %d :\n",n);
-	seq_time_result = print_flops(flops, seq_time);
-	printf("---------------------------------\n");
-	fprintf(log_non_openmp_time_vs_prblmSize, "%d %g\n", n, seq_time_result); //x: problem size, y: time
-	fclose(log_non_openmp_time_vs_prblmSize);
-	/* matrix multiplication with Non-OpenMP ends here */
-
-
 
 	/* Parallelizing matrix multiplication with OpenMP where each cache-block is handled by only one thread starts here*/
-	memset(c, 0, mem_size);
 	time_marker_t time = get_time();
 	#pragma omp parallel default(none) shared(block_size, n, a, b, c) private(i, j, k, ii, jj, kk) reduction(+:num_thread)
 	{
@@ -150,49 +101,11 @@ int main(int argc, char **argv)
 
 	flops = 2.0 * n * n * n;
 
-	printf("OpenMP: Problem size = %d, cache block size = %d, number of threads = %d\n",n, block_size, num_thread);
-	
 	para_time_result = print_flops(flops, time);
+	printf("OpenMP: Problem size = %d, cache block size = %d, num_threads = %d, time=%g\n",
+                                         n, block_size, num_thread, para_time_result);
+
 	/* matrix multiplication with OpenMP ends here */
-
-
-
-
-	/* calculation for parallelizaed measurements */
-	speedup = seq_time_result / para_time_result;
-	eff = speedup / (double)num_thread;
-	printf("speedup = %e, efficiency = %e\n",speedup, eff);
-	printf("---------------------------------\n");
-
-
-
-
-	/* writing the results of parallelizaed calculations in txt files that will be used for plot generation */
-	fprintf(log_openmp_prblmSize_vs_time, "%d %g\n", n, para_time_result); //x: problem size, y: time
-	fprintf(log_openmp_time_vs_threads, "%d %g\n", num_thread, para_time_result); //x: num_thread, y: time
-
-	fprintf(log_openmp_speedup_vs_threads, "%d %g\n", num_thread, speedup); //x: num_thread, y: speedup
-	fprintf(log_ideal_speedup, "%d %d\n", num_thread, num_thread); //x: num_thread, y: ideal speedup=no of processing units
-
-	fprintf(log_openmp_eff_vs_threads, "%d %g\n", num_thread, eff); //x: num_thread, y: effieciency
-	fprintf(log_ideal_eff, "%d %d\n", num_thread, 1); //x: num_thread, y: ideal effieciency
-
-	fprintf(log_openmp_eff_vs_prblm_size, "%d %g\n", n, eff); //x: problem size, y: effieciency
-	fprintf(log_openmp_speedup_vs_prblm_size, "%d %g\n", n, speedup); //x: problem size, y: speedup
-
-
-
-
-	/* closing file descriptors */
-	fclose(log_openmp_prblmSize_vs_time);
-	fclose(log_openmp_time_vs_threads);
-	fclose(log_openmp_speedup_vs_threads);
-	fclose(log_openmp_eff_vs_threads);
-	fclose(log_ideal_speedup);
-	fclose(log_ideal_eff);
-	fclose(log_openmp_eff_vs_prblm_size);
-	fclose(log_openmp_speedup_vs_prblm_size);
-
 
 
 	return(0);
