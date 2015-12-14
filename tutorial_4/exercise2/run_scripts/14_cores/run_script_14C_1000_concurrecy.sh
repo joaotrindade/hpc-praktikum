95902fa0df1b372deb190886972af5ed952f0d5e
João Trindade <joao.ferreira.trindade@gmail.com>
2015-12-14 01:09:39 +0100

    added images. removed old shit

---
+++ tutorial_4/exercise2/run_scripts/14_cores/run_script_14C_1000_concurrecy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -o /home/hpc/t1221/t1221ag/Tutorial4/Ex2/14cores/concurrency/myjob.%j.%N.out 
+#SBATCH -D /home/hpc/t1221/t1221ag/Tutorial4/Ex2/14cores/concurrency
+#SBATCH -J quicksort-analysis
+#SBATCH --get-user-env 
+#SBATCH --clusters=mpp2
+#SBATCH --nodes=1-1 
+#SBATCH --cpus-per-task=14
+# in the line above:
+#  replace 28 by 16 to use the MPP cluster
+#SBATCH --mail-type=end 
+#SBATCH --mail-user=joao.ferreira.trindade@gmail.com 
+#SBATCH --export=NONE 
+#SBATCH --time=00:08:00 
+source /etc/profile.d/modules.sh
+cd mydir
+export OMP_NUM_THREADS=14 
+# 28 is the maximum reasonable value for CooLMUC2, and 16 for the MPP cluster 
+/lrz/mnt/sys.x86_64/intel/amplifier_xe_2015/vtune_amplifier_xe_2015.1.0.367959/bin64/amplxe-cl -collect concurrency -app-working-dir /home/hpc/t1221/t1221ag/Tutorial2/Exercise3 -- /home/hpc/t1221/t1221ag/Tutorial2/Exercise3/quicksort.out 10000 14
